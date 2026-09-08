ac0b3f29b215e724b75de6d4b3728cf2d3a0acf0
Rustysquirrel94 <calebloenser@gmail.com>
2026-09-08 18:22:29 +0000

    my first commit

---
+++ crash-course.py
@@ -0,0 +1,33 @@
+# comment
+
+
+print("The cow jumped over the moon!  ")
+# Variable delareations and Data Types
+
+a = 4    # integer
+b = 5.5  # float
+c = "CSAEA"   # String(s)
+d = False # Boolean
+
+
+print(a, b, c , d)
+
+# Operators
+# + - / *     %  **  ??
+# += -= /=
+# % this thingy called the modules or smthing gives you the remainder of numbers
+e = 21 / 7
+
+print(e)
+e += 7
+print (e)
+
+#f-string
+
+print(f"e is equal to {e}")
+
+
+e -= 5
+e *= 7
+
+print(f"e is currently equal to {e}")
