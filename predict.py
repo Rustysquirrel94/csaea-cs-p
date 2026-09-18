514428a568e9c068ad1c745035bc89580c478adb
Rustysquirrel94 <calebloenser@gmail.com>
2026-09-18 18:24:12 +0000

    bleh

---
+++ predict.py
@@ -0,0 +1,12 @@
+import math 
+f = False 
+t = True 
+nums = [34, 52, 3, 64, 32]
+
+
+print(7 // 2, 7 % 2, -7 // 2)
+print(int(-5.9), math.floor(-5.9))
+print("5" * 3, "22.5" + "5")
+print(2 ** 4, math.pow(2, 4))
+print(True + True + True)
+print(0.1 + 0.2 == 0.3)
