f6bd68047f7991ac66b5f1fbc12d1942fee9e17d
Rustysquirrel94 <calebloenser@gmail.com>
2026-09-24 17:25:13 +0000

    challenges

---
+++ challenges.py
@@ -0,0 +1,24 @@
+# 1
+b = 50 
+b *= 0.20
+(f"The bill is {b}")
+
+print((f"The tip is {b}"))
+
+b += 50
+print(f"The total is {b}")
+
+# 5
+password = "csaea2026"
+attempt = "csaeA2026"
+
+csaea2026 = True
+password == True
+
+if attempt == password:
+ print("Acess Granted")
+else:
+    print("Acess Denied")
+ 
+ 
+
